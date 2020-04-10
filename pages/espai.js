cfda21b27f5467142f3e76f9a275f9fbf424df61
Ramon Gil <ramongil@ramongil.cat>
2020-04-10 14:09:55 +0200

    adding data from fetch

--- pages/espai.js
+++ pages/espai.js
@@ -2,215 +2,199 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
 
-const Espai = () => (
-  <div className='container'>
-    <Head>
-      <title>Espais - Node</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+const Espai = ({ data }) => {
+  const { title, description, moreDescription, content } = data;
+  return (
+    <div className='container'>
+      <Head>
+        <title>{title}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-    <header>
-      <Nav />
-    </header>
+      <header>
+        <Nav />
+      </header>
 
-    <main>
-      <h1 className='title'>Espais - Node</h1>
+      <main>
+        <h1 className='title'>{title}</h1>
 
-      <p className='description'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam faucibus
-        arcu in rhoncus condimentum. Ut tempus, sapien sed rhoncus commodo,
-        magna ex eleifend nisl, id fringilla turpis justo in lorem.
-      </p>
-      <h2>Tipologies d'espais a crear</h2>
-      <h3>En obert - per a tothom || Intern - només per a la comunitat</h3>
-      <div className='grid'>
-        <a className='card'>
-          <header>
-            <img src='/node-esport.svg' alt='Node esport' />
-            <h3>Node Esport &rarr;</h3>
-          </header>
-          <ul>
-            <li>Pràctiva</li>
-            <li>Competició</li>
-          </ul>
-        </a>
-        <a className='card'>
-          <header>
-            <img src='/node-botanic.svg' alt='Node botànic' />
-            <h3>Node Botànic &rarr;</h3>
-          </header>
-          <ul>
-            <li>Jardí</li>
-            <li>Hort</li>
-            <li>Mixtes</li>
-          </ul>
-        </a>
-        <a className='card'>
-          <header>
-            <img src='/node-cultura.svg' alt='Node cultura' />
-            <h3>Node Cultura &rarr;</h3>
-          </header>
-          <ul>
-            <li>Teatre - dansa</li>
-            <li>Música</li>
-            <li>Cinema</li>
-            <li>Expositiu - relat</li>
-          </ul>
-        </a>
-        <a className='card'>
-          <header>
-            <img src='/node-educacio.svg' alt='Node educacio' />
-            <h3>Node Educació &rarr;</h3>
-          </header>
-          <ul>
-            <li>Pràctiva</li>
-            <li>Competició</li>
-          </ul>
-        </a>
-      </div>
-    </main>
-    <Footer />
+        <p className='description'>{description}</p>
 
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        max-width: 85rem;
-        margin: 0 auto;
-      }
+        <p className='description'>
+          <small>{moreDescription}</small>
+        </p>
 
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
+        <div className='grid'>
+          {content.map((c, id) => (
+            <a href='/' className='card' key={c.id} id={id}>
+              <header>
+                <img src={c.icon.url} alt={c.icon.alt} />
+                <h3>{c.title} &rarr;</h3>
+              </header>
+              <p>{c.leadText}</p>
+              {!c.text ? null : (
+                <p>
+                  <small>{c.text}</small>
+                </p>
+              )}
+            </a>
+          ))}
+        </div>
+      </main>
+      <Footer />
 
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 960px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card header {
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-      }
-
-      .card header img {
-        max-width: 60px;
-        padding: 0.5rem;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.25rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
           flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          max-width: 85rem;
+          margin: 0 auto;
         }
-      }
-      @media (min-width: 1024px) {
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+          color: #4af626;
+        }
+
+        .title {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 960px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card header {
+          display: flex;
+          flex-wrap: wrap;
+          align-items: center;
+        }
+
         .card header img {
-          max-width: 75px;
+          max-width: 60px;
+          padding: 0.5rem;
         }
-      }
-    `}</style>
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-);
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.25rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+        @media (min-width: 1024px) {
+          .card header img {
+            max-width: 75px;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://nodeviddata.now.sh/pages.json`);
+  const dataRes = await res.json();
+  return {
+    props: {
+      data: dataRes.find((x) => x.id == 3),
+    },
+  };
+};
 
 export default Espai;
--- pages/index.js
+++ pages/index.js
@@ -1,208 +1,206 @@
+import fetch from 'isomorphic-unfetch';
 import Head from 'next/head';
 import Link from 'next/link';
 import Footer from '../components/Footer';
 import Nav from '../components/Nav';
 
-const Home = () => (
-  <div className='container'>
-    <Head>
-      <title>Benvingut a NodeVid!</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+const Home = ({ data }) => {
+  const { title, content } = data;
+  return (
+    <div className='container'>
+      <Head>
+        <title>Benvingut a {title}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-    <header>
-      <Nav />
-    </header>
+      <header>
+        <Nav />
+      </header>
 
-    <main>
-      <h1 className='title'>
-        Benvingut a <a href='/'>NodeVid!</a>
-      </h1>
+      <main>
+        <h1 className='title'>Benvingut a {title}!</h1>
 
-      <p className='description'>
-        Comença per crear un{' '}
-        <Link href='/espai'>
-          <a>
-            <code>espai</code>
-          </a>
-        </Link>{' '}
-        o afegir-t'hi a{' '}
-        <Link href='/nodes'>
-          <a>
-            <code>un d'ells</code>
-          </a>
-        </Link>
-      </p>
+        <p className='description'>
+          Fes un{' '}
+          <Link href='/equip'>
+            <a>
+              <code>equip</code>
+            </a>
+          </Link>{' '}
+          i creeu un
+          <Link href='/espai'>
+            <a>
+              <code>projecte</code>
+            </a>
+          </Link>{' '}
+          per transformar els teus espais comunitaris en llocs plens de vida.
+        </p>
 
-      <div className='grid'>
-        <a href='/' className='card'>
-          <h3>Què és Nodes &rarr;</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
-            faucibus arcu in rhoncus condimentum. Ut tempus, sapien sed rhoncus
-            commodo, magna ex eleifend nisl, id fringilla turpis justo in lorem.
-          </p>
-        </a>
+        <div className='grid'>
+          {content.map((c, id) => (
+            <a href='/' className='card' key={c.id} id={id}>
+              <h3>{c.title} &rarr;</h3>
+              <p>{c.leadText}</p>
+              {!c.text ? null : (
+                <p>
+                  <small>{c.text}</small>
+                </p>
+              )}
+            </a>
+          ))}
+        </div>
+      </main>
 
-        <a href='/' className='card'>
-          <h3>Qui pot participar &rarr;</h3>
-          <p>
-            In dui odio, consequat ac arcu nec, ullamcorper fermentum lectus.
-            Integer dictum elit augue, vitae consectetur libero ultricies id.
-            Sed maximus placerat lorem vel finibus. Donec auctor lectus quis
-            orci sagittis lacinia.
-          </p>
-        </a>
+      <Footer />
 
-        <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='card'>
-          <h3>Com fer un node &rarr;</h3>
-          <p>
-            Fer un equip >{' '}
-            <img src='/busqueu-afinitats.svg' alt='Busqueu afinitats' /> Busqueu
-            afinitats > <img src='/escolliu-node.svg' alt='Busqueu afinitats' />{' '}
-            Escolliu Node > <img src='/construiu.svg' alt='Busqueu afinitats' />{' '}
-            Construiu > Gaudiu > Guanyeu
-          </p>
-        </a>
-      </div>
-    </main>
-
-    <Footer />
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        max-width: 85rem;
-        margin: 0 auto;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:last-child {
-        flex-basis: 100%;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.25rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      .card img {
-        max-width: 50px;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
           flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          max-width: 85rem;
+          margin: 0 auto;
         }
-      }
-    `}</style>
 
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-);
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+          color: #4af626;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          color: #000000;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 100%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.25rem;
+          color: #4af626;
+        }
+
+        .card p {
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .card p:last-child {
+          margin: 0;
+        }
+
+        .card img {
+          max-width: 50px;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          color: #ffffff;
+          background: #000000;
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch(`https://nodeviddata.now.sh/pages.json`);
+  const dataRes = await res.json();
+  return {
+    props: {
+      data: dataRes.find((x) => x.id == 1),
+    },
+  };
+};
 
 export default Home;
