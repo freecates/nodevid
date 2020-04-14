4387358313995f7f01de13706fd39e3ed79543d6
ramon.gil <ramon.gil@deporvillage.com>
2020-04-14 16:25:51 +0200

    added GA

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import Router from 'next/router'
+
+import * as gtag from '../lib/gtag'
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+
+export default ({ Component, pageProps }) => {
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  )
+}
